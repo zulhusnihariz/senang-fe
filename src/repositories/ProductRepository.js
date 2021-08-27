03654cf07290f16469c08c2c2c575080f66b5eb7
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-08-27 15:08:11 +0800

    update ProductRepository

--- src/repositories/ProductRepository.js
+++ src/repositories/ProductRepository.js
@@ -6,8 +6,23 @@ export default {
   getAllProduct() {
     return axios.get(`${baseURL}/`);
   },
-  filterProductByCategory(data) {
-    return axios.get(`${baseURL}?category_id=${data.category_id}`);
+  filterProduct(data) {
+    const query = Object.keys(data);
+    let dynamicURL = `${baseURL}`;
+
+    query.forEach((el, index) => {
+      if (index === 0) dynamicURL = dynamicURL.concat(`?${el}=${data[el]}`);
+      if (index > 0) dynamicURL = dynamicURL.concat(`&${el}=${data[el]}`);
+    });
+
+    console.log(dynamicURL);
+
+    return axios.get(dynamicURL);
+
+    // return axios.get(`${baseURL}?category_id=${data.category_id}`);
+  },
+  getProductByTextSearch(text) {
+    return axios.get(`${baseURL}/search/${text}`);
   },
   getAllCategory() {
     return axios.get(`${baseURL}/category`);
