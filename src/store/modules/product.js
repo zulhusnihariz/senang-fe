b93cd3c666bc54f0bb1a43ef14a988c309a69840
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-08-31 13:46:43 +0800

    update product vuex store

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -3,6 +3,7 @@
 const state = {
   products: [],
   categories: [],
+  currentCart: JSON.parse(localStorage.getItem('cart')) || [],
 };
 const getters = {
   products: state => {
@@ -10,6 +11,10 @@ const getters = {
   },
   categories: state => {
     return state.categories;
+  },
+
+  currentCart: state => {
+    return state.currentCart;
   },
 };
 const mutations = {
@@ -19,15 +24,43 @@ const mutations = {
   setCategories(state, payload) {
     state.categories = payload;
   },
+
+  setCurrentCart(state, payload) {
+    console.log(state.currentCart);
+    const index = state.currentCart.findIndex(el => el.productId === payload.productId);
+
+    // Check for existing cart in local storage
+    if (!!state.currentCart.length && index >= 0) {
+      // if cart exists and product is currently inside the cart
+      // update payload quantity to be the same as existing quantity
+      // remove existing product data and replace with the new product data
+      payload.quantity = state.currentCart[index].quantity + 1;
+      state.currentCart.splice(index, 1, payload); // remove 1 item at given index and replace with payload
+    } else if (!!state.currentCart.length && index === -1) {
+      // if cart exists but product is currently not inside the cart
+      // set product quantity to 1 and add the new product to existing cart
+      payload.quantity = 1;
+      state.currentCart = [...state.currentCart, payload];
+    } else {
+      // if cart does not exists in local storage, send product payload as array
+      payload.quantity = 1;
+      state.currentCart = [payload];
+    }
+
+    localStorage.setItem('cart', JSON.stringify(state.currentCart));
+  },
 };
 const actions = {
   async setProducts({ commit }, payload) {
     commit('setProducts', payload);
   },
   async setCategories({ commit }, payload) {
-    console.log('inside store', payload);
-
     commit('setCategories', payload);
+  },
+  async addProductToCart({ commit }, payload) {
+    commit('setCurrentCart', payload);
+
+    // Check if product id exists
   },
 };
 
